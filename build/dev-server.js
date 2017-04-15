497ad6a0101f3cf13b6f6afdbd9043cd84d235d2
宋均均 <sjj107294@alibaba-inc.com>
2017-04-15 12:05:02 +0800

    feat: 修改构建

--- build/dev-server.js
+++ build/dev-server.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const webpackConf = require('./webpack.dev.conf');
 const ora = require('ora');
 
+console.log('***开始构建***');
 const spinner = ora('构建中...');
 spinner.start();
 
@@ -14,7 +15,10 @@ webpack(webpackConf, (err, stats) => {
   // 错误信息
   const errors = stats.compilation.errors;
   if (errors) {
-    return console.log(errors);
+    return errors.forEach((error) => {
+      const errInfo = typeof error === 'object' ? error.message : error;
+      console.log(`发生错误: ${errInfo}`);
+    });
   }
   console.log('构建成功');
 });
