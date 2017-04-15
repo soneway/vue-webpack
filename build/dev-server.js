fd0576d95e5af2bcb00327e4001dc5c1c215a76a
宋均均 <sjj107294@alibaba-inc.com>
2017-04-15 11:17:05 +0800

    feat: 修改dev配置

--- build/dev-server.js
+++ build/dev-server.js
@@ -5,9 +5,16 @@ const ora = require('ora');
 const spinner = ora('构建中...');
 spinner.start();
 
-console.log('开始构建');
-webpack(webpackConf, (err) => {
+webpack(webpackConf, (err, stats) => {
   spinner.stop();
+
+  // 抛出错误
   if (err) throw err;
+
+  // 错误信息
+  const errors = stats.compilation.errors;
+  if (errors) {
+    return console.log(errors);
+  }
   console.log('构建成功');
 });
