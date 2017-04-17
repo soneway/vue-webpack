3f6df27ef77f540bc365c57948d88951fda82e95
宋均均 <sjj107294@alibaba-inc.com>
2017-04-17 19:09:39 +0800

    feat: 添加scss支持

---
+++ src/components/scss.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrapper">
+        test
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .wrapper {
+        width: 300px;
+        height: 200px;
+        background: rgba(#000, 0.5);
+    }
+</style>
+
+<script>
+
+  export default {
+    data() {
+      return {};
+    },
+    created() {
+
+    },
+    methods: {}
+  }
+</script>
