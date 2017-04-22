d2a0d5933cbd3b69eb1d8e455c9df2fde110d022
宋均均 <sjj107294@alibaba-inc.com>
2017-04-22 21:21:24 +0800

    feat: 打包脚本

--- build/webpack.dist.conf.js
+++ build/webpack.dist.conf.js
@@ -1,25 +1,43 @@
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 const srcHelper = require('./src-helper');
 
 // 基础配置
 const webpackConf = require('./webpack.base.conf');
-// html模板
+
+// html处理
 const templates = srcHelper.getTemplate();
 const templatePlugins = templates.map((item) => {
-  item.minify = true;
+  Object.assign(item, {
+    // html压缩配置
+    minify: {
+      minifyCSS: true,
+      minifyJS: true,
+      removeComments: true,
+      collapseWhitespace: true
+    }
+  });
   return new HtmlWebpackPlugin(item);
 });
 
-// dev配置
+// js压缩
+const uglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
+  compress: {
+    warnings: true
+  }
+});
+
+// dist配置
 module.exports = webpackMerge(webpackConf, {
   watch: true,
   output: {
     path: path.join(__dirname, '../dist'),
-    filename: '[name].[chunck].js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
-    ...templatePlugins
+    ...templatePlugins,
+    uglifyJsPlugin
   ]
 });
